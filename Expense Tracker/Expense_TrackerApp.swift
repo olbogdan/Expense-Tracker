4fce542eb806ca0a4ea00d01188d35baa4af2073
bogdanov <iceposhta@gmail.com>
2021-05-06 22:39:57 +0100

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by bogdanov on 06.05.21.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
